eaf4909d5f55bc62ee185d5a7d2981441e6e7ca9
bcperry <blainecperry@gmail.com>
2022-02-23 16:46:13 -0500

    Updated for environment issues

--- coco_eval.py
+++ coco_eval.py
@@ -3,7 +3,6 @@ import io
 from contextlib import redirect_stdout
 
 import numpy as np
-import pycocotools.mask as mask_util
 import torch
 import utils
 from pycocotools.coco import COCO
@@ -83,40 +82,6 @@ class CocoEvaluator:
                         "score": scores[k],
                     }
                     for k, box in enumerate(boxes)
-                ]
-            )
-        return coco_results
-
-    def prepare_for_coco_segmentation(self, predictions):
-        coco_results = []
-        for original_id, prediction in predictions.items():
-            if len(prediction) == 0:
-                continue
-
-            scores = prediction["scores"]
-            labels = prediction["labels"]
-            masks = prediction["masks"]
-
-            masks = masks > 0.5
-
-            scores = prediction["scores"].tolist()
-            labels = prediction["labels"].tolist()
-
-            rles = [
-                mask_util.encode(np.array(mask[0, :, :, np.newaxis], dtype=np.uint8, order="F"))[0] for mask in masks
-            ]
-            for rle in rles:
-                rle["counts"] = rle["counts"].decode("utf-8")
-
-            coco_results.extend(
-                [
-                    {
-                        "image_id": original_id,
-                        "category_id": labels[k],
-                        "segmentation": rle,
-                        "score": scores[k],
-                    }
-                    for k, rle in enumerate(rles)
                 ]
             )
         return coco_results
