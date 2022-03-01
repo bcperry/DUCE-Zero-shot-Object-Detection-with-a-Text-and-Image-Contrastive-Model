26e6d5924ba0bdc349eca4ccc92dac8723df073d
bcperry <blainecperry@gmail.com>
2022-02-28 20:00:57 -0500

    engine bug fix
    Corrected issue in engine where model graph was being saved an extra time

--- engine.py
+++ engine.py
@@ -61,7 +61,7 @@ def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq, sc
             if lr_scheduler is not None:
                 lr_scheduler.step()
 
-        metric_logger.update(loss=losses_reduced, **loss_dict_reduced)
+        metric_logger.update(loss=losses_reduced.item(), **loss_dict_reduced)
         metric_logger.update(lr=optimizer.param_groups[0]["lr"])
 
     return metric_logger
