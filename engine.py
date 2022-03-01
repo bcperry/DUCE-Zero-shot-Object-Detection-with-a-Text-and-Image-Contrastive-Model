8837d646653c11792843cbd8139c36cc70e6d8ca
bcperry <blainecperry@gmail.com>
2022-03-01 11:25:38 -0500

    Updates to engine to allow for half precision evaluation

--- engine.py
+++ engine.py
@@ -38,15 +38,12 @@ def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq, sc
             loss_dict = model(images, targets)
             losses = sum(loss for loss in loss_dict.values())
 
-        # reduce losses over all GPUs for logging purposes
-        loss_dict_reduced = utils.reduce_dict(loss_dict)
-        losses_reduced = sum(loss for loss in loss_dict_reduced.values())
 
-        loss_value = losses_reduced.item()
+        loss_value = losses.item()
 
         if not math.isfinite(loss_value):
             print(f"Loss is {loss_value}, stopping training")
-            print(loss_dict_reduced)
+            print(loss_dict)
             sys.exit(1)
         if training:
             optimizer.zero_grad()
@@ -61,7 +58,7 @@ def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq, sc
             if lr_scheduler is not None:
                 lr_scheduler.step()
 
-        metric_logger.update(loss=losses_reduced.item(), **loss_dict_reduced)
+        metric_logger.update(loss=loss_value, **loss_dict)
         metric_logger.update(lr=optimizer.param_groups[0]["lr"])
 
     return metric_logger
@@ -100,8 +97,8 @@ def evaluate(model, data_loader, device):
         if torch.cuda.is_available():
             torch.cuda.synchronize()
         model_time = time.time()
-
-        outputs = model(images)
+        with autocast():
+            outputs = model(images)
 
 
         outputs = [{k: v.to(cpu_device) for k, v in t.items()} for t in outputs]
@@ -122,6 +119,7 @@ def evaluate(model, data_loader, device):
     coco_evaluator.accumulate()
     coco_evaluator.summarize()
     torch.set_num_threads(n_threads)
+    del outputs
     return coco_evaluator
 
 from torch.utils.tensorboard import SummaryWriter
